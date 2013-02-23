2ed79c3d4b922b60a4b080569f904f4a7138e8c1
Horacio G. de Oro <hgdeoro@gmail.com>
2013-02-23 16:34:44 -0300

    Add OptimisticLockingError

--- django/db/utils.py
+++ django/db/utils.py
@@ -21,6 +21,8 @@ class DatabaseError(Exception):
 class IntegrityError(DatabaseError):
     pass
 
+class OptimisticLockingError(IntegrityError):
+    pass
 
 def load_backend(backend_name):
     # Look for a fully qualified database backend name
